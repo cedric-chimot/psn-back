ea9112a0ed50af23680f9c9ec9b676cf5a8650b1
Cedric Chimot <cedric_om10@yahoo.fr>
2025-03-12 00:16:55 +0100

    Initial commit

---
+++ src/main/java/fr/cch/trophees_psn/TropheesPsnApplication.java
@@ -0,0 +1,13 @@
+package fr.cch.trophees_psn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TropheesPsnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TropheesPsnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/cch/trophees_psn/TropheesPsnApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.cch.trophees_psn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TropheesPsnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

08e529e05d524217488233a748457c86189d9a70
Cedric Chimot <cedric_om10@yahoo.fr>
2025-04-23 10:52:32 +0200

    ajout méthodes service et controller stats niveaux

--- src/main/java/fr/cch/trophees_psn/controller/StatsNiveauxController.java
+++ src/main/java/fr/cch/trophees_psn/controller/StatsNiveauxController.java
@@ -1,11 +1,87 @@
 package fr.cch.trophees_psn.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import fr.cch.trophees_psn.entity.StatsNiveaux;
+import fr.cch.trophees_psn.service.StatsNiveauxService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/statsNiveaux")
 @CrossOrigin
 public class StatsNiveauxController {
+
+  /**
+   * Variable pour appeler le service
+   */
+  public final StatsNiveauxService statsNiveauxService;
+
+  /**
+   * Le constructeur
+   * @param statsNiveauxService injection du service statistique de niveau
+   */
+  public StatsNiveauxController(StatsNiveauxService statsNiveauxService) {
+    this.statsNiveauxService = statsNiveauxService;
+  }
+
+  /**
+   * Créer une nouvelle statistique
+   * @param statsNiveaux la statistique à créer
+   * @return la nouvelle statistique
+   */
+  @PostMapping("/create")
+  public ResponseEntity<StatsNiveaux> createStatsNiveaux(@RequestBody StatsNiveaux statsNiveaux) {
+    StatsNiveaux savedStatsNiveaux = statsNiveauxService.statsNiveauxSave(statsNiveaux);
+    return ResponseEntity.ok(savedStatsNiveaux);
+  }
+
+  /**
+   * Afficher toutes les statistiques de niveaux
+   * @return la liste des statistiques de niveaux
+   */
+  @GetMapping("/all")
+  public List<StatsNiveaux> getAllStatsNiveaux() {
+    return statsNiveauxService.findAllStatsNiveaux();
+  }
+
+  /**
+   * Rechercher une statistique par son id
+   *
+   * @param id l'id de la statistique
+   * @return la statistique trouvée
+   */
+  @GetMapping("/{id}")
+  public StatsNiveaux getStatsNiveauxById(@PathVariable Long id) {
+    return statsNiveauxService.findStatsNiveauxById(id);
+  }
+
+  /**
+   * Mettre à jour une statistique
+   * @param statsNiveaux la statistique à mettre à jour
+   * @return la statistique mise à jour
+   */
+  @PatchMapping("/update")
+  public StatsNiveaux updateStatsNiveaux(@RequestBody StatsNiveaux statsNiveaux) {
+    return statsNiveauxService.updateStatsNiveaux(statsNiveaux);
+  }
+
+  /**
+   * Supprimer une statistique par son id
+   * @param id l'id d'une statistique à supprimer
+   * @return la statistique supprimée
+   */
+  @DeleteMapping("/delete/{id}")
+  public StatsNiveaux deleteStatsNiveauxById(@PathVariable Long id) {
+    return statsNiveauxService.deleteById(id);
+  }
+
+  /**
+   * Supprimer toutes les statistiques
+   */
+  @DeleteMapping("/delete/all")
+  public void deleteAllStatsNiveaux() {
+    statsNiveauxService.deleteAll();
+  }
+
 }
--- src/main/java/fr/cch/trophees_psn/service/StatsNiveauxService.java
+++ src/main/java/fr/cch/trophees_psn/service/StatsNiveauxService.java
@@ -1,9 +1,99 @@
 package fr.cch.trophees_psn.service;
 
+import fr.cch.trophees_psn.entity.StatsNiveaux;
+import fr.cch.trophees_psn.exceptions.CustomException;
+import fr.cch.trophees_psn.repository.StatsNiveauxRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
 public class StatsNiveauxService {
+
+  /**
+   * Variable pour appeler le répository
+   */
+  private final StatsNiveauxRepository statsNiveauxRepository;
+
+  /**
+   * Constructeur du service
+   * @param statsNiveauxRepository le répo des statistiques de niveau
+   */
+  public StatsNiveauxService(StatsNiveauxRepository statsNiveauxRepository) {
+    this.statsNiveauxRepository = statsNiveauxRepository;
+  }
+
+  /**
+   * Ajouter une statistique
+   * @param statsNiveaux la statistique à ajouter
+   * @return la nouvelle statistique
+   */
+  public StatsNiveaux statsNiveauxSave(StatsNiveaux statsNiveaux) {
+    return statsNiveauxRepository.save(statsNiveaux);
+  }
+
+  /**
+   * Trouver toutes les statistiques
+   * @return la liste de toutes les statistiques
+   */
+  public List<StatsNiveaux> findAllStatsNiveaux() {
+    return statsNiveauxRepository.findAll();
+  }
+
+  /**
+   * Trouver une statistique par son Id
+   * @param id l'id de la statsNiveaux
+   * @return la statsNiveaux trouvé
+   */
+  public StatsNiveaux findStatsNiveauxById(Long id) {
+    return statsNiveauxRepository.findById(id)
+      .orElseThrow(() -> new CustomException("StatsNiveaux", "id", id));
+  }
+
+  /**
+   * Mettre à jour une stat
+   * @param statsNiveaux la statistique à mettre à jour
+   * @return la statistique mise à jour
+   */
+  public StatsNiveaux updateStatsNiveaux(StatsNiveaux statsNiveaux) {
+    Optional<StatsNiveaux> existingStatsNiveaux = statsNiveauxRepository.findById(statsNiveaux.getId());
+
+    if(existingStatsNiveaux.isPresent()) {
+      StatsNiveaux existingStats = existingStatsNiveaux.get();
+
+      existingStats.setNiveau(statsNiveaux.getNiveau());
+      existingStats.setNiveauAnnee(statsNiveaux.getNiveauAnnee());
+      return statsNiveauxRepository.save(existingStats);
+    } else {
+      throw new CustomException("Le statsNiveaux est inconnu", "id", statsNiveaux.getId());
+    }
+  }
+
+  /**
+   * Supprimer une statistique par son id
+   * @param id l'id de la statistique à supprimer
+   * @return la statistique supprimée
+   */
+  public StatsNiveaux deleteById(Long id) {
+    Optional<StatsNiveaux> optionalStatsNiveaux = statsNiveauxRepository.findById(id);
+
+    if (optionalStatsNiveaux.isPresent()) {
+      StatsNiveaux statsNiveaux = optionalStatsNiveaux.get();
+      statsNiveauxRepository.delete(statsNiveaux);
+      return statsNiveaux;
+    } else {
+      throw new CustomException("StatsNiveaux", "id",  id);
+    }
+  }
+
+  /**
+   * Supprimer toutes les statistiques de niveau
+   */
+  public void deleteAll() {
+    statsNiveauxRepository.deleteAll();
+  }
+
 }

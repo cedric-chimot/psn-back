ea287e86dddfe28695e0a0b77c78212249006c07
Cedric Chimot <cedric_om10@yahoo.fr>
2025-05-12 11:11:59 +0200

    ajout du controller Jeux/Plateforme

---
+++ src/main/java/fr/cch/trophees_psn/controller/JeuxPlatefromeController.java
@@ -0,0 +1,12 @@
+package fr.cch.trophees_psn.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/jeuxPlateforme")
+@CrossOrigin
+public class JeuxPlatefromeController {
+
+}
